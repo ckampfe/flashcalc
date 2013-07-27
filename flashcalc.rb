c5b0a741e165185b728d18f17ecde46be3810413
Clark Kampfe <clark.kampfe@gmail.com>
2013-07-27 18:19:05 -0500

    Added distance math, low ISO compatibility
    Now works with ISOs below 100, for those slow B&W films. Added distance
    calculator math.

--- flashcalc.rb
+++ flashcalc.rb
@@ -1,4 +1,3 @@
-
 def prompt()
   print "> "
 end # ask which calculation: exposure distance or minimum aperture
@@ -6,8 +5,8 @@ end # ask which calculation: exposure distance or minimum aperture
 
 def menu()
   puts "Which would you like to do:"
-  puts "1. Get max. distance to subject"
-  puts "2. Get minimum aperture"
+  puts "1 :: Get max. distance to subject"
+  puts "2 :: Get minimum aperture"
  
   prompt; choice = STDIN.gets.chomp
   if choice == "1"
@@ -20,15 +19,22 @@ def menu()
   end
 end
 
+
 # calculate acceptable distance to achieve proper exposure at a given aperture, GN, and ISO.
+# At this point all values given are for flash at 1/1 power
 def calc_distance()
   puts "GN?"; prompt 
   gn = STDIN.gets.chomp.to_i
-  puts "Aperture?"; prompt
-  ap = STDIN.gets.chomp.to_i
+  puts "Aperture? (f-number)"; prompt
+  ap = STDIN.gets.chomp.to_f
   puts "ISO?"; prompt
   iso = STDIN.gets.chomp.to_i
+
+  # calculate maximum distance to achieve proper exposure given aperture (f-number), ISO, and GN
+  dist = gn / ap
+  puts "Distance: #{dist.round()}ft @ f#{ap.round(1)}, ISO #{iso}, GN #{gn}"
 end
+
 
 # calculate the minimum aperture to achieve proper exposure at a given distance with a given ISO and GN.
 # At this point all values given are for flash at 1/1 power
@@ -41,17 +47,15 @@ def calc_aperture()
   iso = STDIN.gets.chomp.to_i
   
   # transmute ISO value into something useful (raw stops) 
-  iso_modifier = Math.log(iso/100, 2) 
-  puts "ISO modifier is #{iso_modifier} stops"
+  iso_modifier = Math.log(iso/100.0, 2) 
+  puts "ISO modifier is #{iso_modifier.round(1)} stops"
   
   # calculate aperture using ISO modifier instead of base ISO 100
   # ap_test = gn / dist
   # puts "iso 100 value = #{ap_test}"
   ap = (gn / dist) * ((2 ** 0.5) ** iso_modifier)
-  puts "Aperture: f#{ap.round(1)}, ISO #{iso}, GN: #{gn}, Distance (ft): #{dist}"
-
+  puts "Minimum aperture: f#{ap.round(1)} @ ISO #{iso}, GN: #{gn}, Distance (ft): #{dist}"
 end
-
 
 
 puts menu()

699e5f1ae1b527d44512f5671860e6e9e7f1363c
Clark Kampfe <clark.kampfe@gmail.com>
2013-07-27 15:58:13 -0500

    Added aperture math
    ISO calculation and apertures seem to be correct. Added tenths place
    precision. Assuming flash power is 1/1.

--- flashcalc.rb
+++ flashcalc.rb
@@ -1,27 +1,26 @@
+
 def prompt()
   print "> "
-end
+end # ask which calculation: exposure distance or minimum aperture
 
-# ask which they want to calculate: exposure distance or minimum aperture
+
 def menu()
   puts "Which would you like to do:"
-  puts "1. Get distance to subject"
+  puts "1. Get max. distance to subject"
   puts "2. Get minimum aperture"
  
   prompt; choice = STDIN.gets.chomp
   if choice == "1"
     calc_distance
-  elsif choice.include? "2"
+  elsif choice == "2"
     calc_aperture
   else
     puts "Try again."
     menu
   end
-    
-
 end
 
-# calculate acceptable distance to achieve proper exposure at a given aperture
+# calculate acceptable distance to achieve proper exposure at a given aperture, GN, and ISO.
 def calc_distance()
   puts "GN?"; prompt 
   gn = STDIN.gets.chomp.to_i
@@ -29,18 +28,28 @@ def calc_distance()
   ap = STDIN.gets.chomp.to_i
   puts "ISO?"; prompt
   iso = STDIN.gets.chomp.to_i
-
 end
 
-# calculate the minimum aperture to achieve proper exposure at a given distance
+# calculate the minimum aperture to achieve proper exposure at a given distance with a given ISO and GN.
+# At this point all values given are for flash at 1/1 power
 def calc_aperture()
   puts "GN?"; prompt 
   gn = STDIN.gets.chomp.to_i
-  puts "Distance?"; prompt
+  puts "Approximate distance to subject? (ft)"; prompt
   dist = STDIN.gets.chomp.to_i
   puts "ISO?"; prompt
   iso = STDIN.gets.chomp.to_i
   
+  # transmute ISO value into something useful (raw stops) 
+  iso_modifier = Math.log(iso/100, 2) 
+  puts "ISO modifier is #{iso_modifier} stops"
+  
+  # calculate aperture using ISO modifier instead of base ISO 100
+  # ap_test = gn / dist
+  # puts "iso 100 value = #{ap_test}"
+  ap = (gn / dist) * ((2 ** 0.5) ** iso_modifier)
+  puts "Aperture: f#{ap.round(1)}, ISO #{iso}, GN: #{gn}, Distance (ft): #{dist}"
+
 end
 
 
